ab991cc67ab4d1b0aebefe8d23b79f435f7e04ad
C0staaa <guilhermemartinscosta993@gmail.com>
2025-10-23 09:53:24 +0100

    Função dividir

--- c_calcular.c
+++ c_calcular.c
@@ -7,6 +7,16 @@ int sum(int n1, int n2){
 int subtrair(int n1, int n2){
     return n1 - n2;
 }
+
+float div(int n1, int n2){
+    if (n1 || n2 == 0){
+        printf("Erro! \n");
+        return 0;
+    } else {
+        return n1 / n2;
+    }
+}
+
 int main(){
 
     int n1, n2;
@@ -14,9 +24,13 @@ int main(){
     scanf("%d %d", &n1, &n2);
 
     int soma = sum(n1, n2);
+    printf("A soma é %d \n", soma);
+
     int subtracao = subtrair(n1, n2);
-    printf("A soma é %d", soma);
-    printf("A subtração é %d", subtrair);
+    printf("A subtração é %d \n", subtracao);
+
+    float divisao = div(n1, n2);
+    printf("A divisão é %2.f \n", divisao);
 
     return 0;
 }
