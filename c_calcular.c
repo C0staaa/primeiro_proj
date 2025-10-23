01ef4488ad0f94949d5087326b30f7cb1eea8e26
C0staaa <guilhermemartinscosta993@gmail.com>
2025-10-23 09:19:34 +0100

    Adicionar função para somar e chamar a mesma

--- c_calcular.c
+++ c_calcular.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+int sum(int n1, int n2){
+    return n1 + n2;
+}
+
 int main(){
-        
+    
     int n1, n2;
     printf("Insira dois valores inteiros separados por espaço: ");
     scanf("%d %d", &n1, &n2);
+
+    int soma = sum(n1, n2);
+    printf("A soma é %d", soma);
 
     return 0;
 }
